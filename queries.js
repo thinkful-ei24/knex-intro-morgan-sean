bccff460f9a6f198e508bec9d09c9118fe97f03f
Sean Phelan <seanphelan97@gmail.com>
2018-09-11 09:45:30 -0600

    added drills

--- queries.js
+++ queries.js
@@ -7,9 +7,32 @@ const knex = require('knex')(DATABASE);
 process.stdout.write('\x1Bc');
 
 // Sample select 
+// knex
+//   .select()
+//   .from('restaurants')
+//   .limit(2)
+//   .debug(true)
+//   .then(results => console.log(results));
+
+knex.select().from('restaurants')
+  .then(results => console.log(results));
+
 knex
   .select()
   .from('restaurants')
-  .limit(2)
-  .debug(true)
+  .where('cuisine', 'Italian')
   .then(results => console.log(results));
+
+knex
+  .select('id', 'name')
+  .from('restaurants')
+  .limit(10)
+  .then(results => console.log(results));
+
+knex
+  .count()
+  .from('restaurants')
+  .where('cuisine', 'Thai')
+  .then(count => console.log(count));
+
+  
